12501313091b5dab765821c2b0b2f9b5d1fc91da
mustreff115-cyber <mustreff115@gmail.com>
2026-08-21 17:58:31 +0100

    Update datadrama.js

--- datadrama.js
+++ datadrama.js
@@ -1,27 +1,18 @@
-const dramaData = {
-    title: "موقع الدراما",
-    series: [
-        {
-            id: "EXO",
-            name: "EXO",
-            episodes: [
-                {
-                    episodeNumber: 1,
-                    title: "الحلقة 1",
-                    parts: [
-                        "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/278.0.1786720022815.v3.mp4",
-                        "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/279.0.1786720023245.v3.mp4"
-                    ]
-                },
-                {
-                    episodeNumber: 2,
-                    title: "الحلقة 2",
-                    parts: [
-                        "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/002/437.0.1786723845330.v3.mp4",
-                        "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/002/438.0.1786723846027.v3.mp4"
-                    ]
-                }
+const dramaDataset = [
+    {
+        id: 34567,
+        Serie: "EXO",
+        Desc: "مسلسل EXO وحلقاته",
+        NbEp: 2,
+        Episode: {
+            Ep1_Seg: [
+                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/278.0.1786720022815.v3.mp4",
+                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/279.0.1786720023245.v3.mp4"
+            ],
+            Ep2_Seg: [
+                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/437.0.1786723845330.v3.mp4",
+                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/438.0.1786723846027.v3.mp4"
             ]
         }
-    ]
-};
+    }
+];
