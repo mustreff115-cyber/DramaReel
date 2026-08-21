30dedbe87db8db9bdd73aa291ff8af74ee6bbcaf
mustreff115-cyber <mustreff115@gmail.com>
2026-08-21 18:14:02 +0100

    Update datadrama.js

--- datadrama.js
+++ datadrama.js
@@ -10,8 +10,8 @@ const dramaDataset = [
                 "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/279.0.1786720023245.v3.mp4"
             ],
             Ep2_Seg: [
-                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/437.0.1786723845330.v3.mp4",
-                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/001/438.0.1786723846027.v3.mp4"
+                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/002/437.0.1786723845330.v3.mp4",
+                "https://raw.githubusercontent.com/mustreff115-cyber/DramaReel/main/EXO/Ep/002/438.0.1786723846027.v3.mp4"
             ]
         }
     }
